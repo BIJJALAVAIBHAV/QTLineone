26c7ae78dd62a82810734afd8c79a7ad395d33a2
BIJJALA VAIBHAV <92683482+BIJJALAVAIBHAV@users.noreply.github.com>
2024-05-11 06:36:42 +0000

    Pending changes exported from your codespace

--- QT_Lineone/settings.py
+++ QT_Lineone/settings.py
@@ -79,12 +79,9 @@ WSGI_APPLICATION = 'QT_Lineone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qt_lineone_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+     
     }
 }
 
